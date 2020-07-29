de0b05f2ea8d1fb9ba4b8029761515c91728ea52
Alvis <alvis.mach@quirqy.com>
2020-07-29 19:53:48 +1000

    Initial Commit

---
+++ BlakeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlakeApp
+//
+//  Created by suitecontrol on 29/7/20.
+//  Copyright © 2020 alvis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
